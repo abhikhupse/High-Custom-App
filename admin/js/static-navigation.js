060444ac5897c9e9ab0a18dc07bd892e3bcdbf33
abhikhupse <abhikhupse2001@gmail.com>
2026-09-16 10:29:23 +0530

    Make shared sidebar override Master legacy styles

--- admin/js/static-navigation.js
+++ admin/js/static-navigation.js
@@ -28,7 +28,7 @@
     sidebarCss.id = "adminSharedSidebarCss";
     sidebarCss.rel = "stylesheet";
     // Versioned so every legacy static page receives the current shared shell.
-    sidebarCss.href = `${new URL("css/admin-shared-sidebar.css", getAdminRoot()).href}?v=20260914-3`;
+    sidebarCss.href = `${new URL("css/admin-shared-sidebar.css", getAdminRoot()).href}?v=20260916-1`;
     document.head.append(sidebarCss);
   }
 
